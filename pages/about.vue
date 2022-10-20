1d84bf1230aaa0cfca0a8c52482be3f64c96220d
wesngu28 <serpents123@gmail.com>
2022-10-20 13:40:52 -0700

    resolve import

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <video autoplay loop class="lux">
-            <source src="1.mp4" type="video/mp4" />
+            <source src="/1.mp4" type="video/mp4" />
         </video>
         <NuxtPage />
     </div>
